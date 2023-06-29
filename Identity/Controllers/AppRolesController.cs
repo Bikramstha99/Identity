42ccdb1b5754d92251cc75d8a23fc0f3ffaff829
Bikram Shrestha <stha.bikram999@gmail.com>
2023-06-29 11:08:02 +0545

    add Roles Controller

--- Identity/Controllers/AppRolesController.cs
+++ Identity/Controllers/AppRolesController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 namespace Identity.Controllers
 {
     public class AppRolesController : Controller
     {
+        private readonly RoleManager<IdentityRole> rolemanager;
+
+        public AppRolesController(RoleManager<IdentityRole> rolemanager)
+        {
+            this.rolemanager = rolemanager;
+        }
+
+        //list all the roles created
         public IActionResult Index()
+        {
+            var roles = rolemanager.Roles;
+            return View(roles);
+        }
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+        [HttpGet]
+        public async Task<IActionResult> Create(IdentityRole model)
+        {
+            if(!rolemanager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())
+            {
+                rolemanager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");  
+            
+        }
+
+
+
     }
 }
